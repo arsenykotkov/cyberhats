6022ffca6a4d50bf441c691ae13ce711b9e83f72
Arseny Kotkov <arsenykotkov@gmail.com>
2022-07-09 22:51:33 +1000

    Add color and visibility changing, pass nodes from Cap to App.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Suspense, useRef, useState } from "react";
+import React, { Suspense, useState } from "react";
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
 import Cap from "./Cap";
+import { Button, Collapse } from "antd";
+import { CirclePicker } from "react-color";
+const { Panel } = Collapse;
 
 export default function App() {
   const [visorColor, setVisorColor] = useState('#ffffff');
+  const [autoRotationEnabled, setAutoRotationEnabled] = useState(true);
+  const [capeVisibility, setCapeVisibility] = useState(true);
+
+  function modelHandler(data) {
+    console.log(data)
+  }
+
   return (
     <>
-      <input type='color' id='visorColor' name='visorColor' value={visorColor} onChange={event => setVisorColor(event.target.value)} />
-      <Canvas style={{height: '100vh'}}>
+      <Canvas style={{ height: '100vh', position: 'absolute' }}>
         <OrbitControls
-          autoRotate={true}
+          autoRotate={autoRotationEnabled}
           autoRotateSpeed={2}
         />
         <Suspense> {/* Catch errors */}
@@ -22,14 +31,47 @@ export default function App() {
             angle={0.1}
             castShadow
           />
-          <Cap customColors={{
-            crown: 'white',
-            topPoint: 'grey',
-            visor: visorColor,
-            cape: 'skyblue'
-          }}/>
+          <Cap
+            onChange={modelHandler}
+            customColors={{
+              crown: 'white',
+              topPoint: 'grey',
+              visor: visorColor,
+              cape: 'skyblue'
+            }}
+            visibleObjects={{
+              cape: capeVisibility
+            }}
+          />
         </Suspense>
       </Canvas>
+      <Collapse accordion style={{ width: '230px' }}>
+        <Panel header="Козырёк" key={1} style={{ overflow: 'auto' }}>
+          <CirclePicker width="auto" onChange={color => setVisorColor(color.hex)} />
+        </Panel>
+        <Panel header="Отлетная часть" key={2}>
+          <Button
+            onClick={() => {
+              setCapeVisibility(capeVisibility ? false : true);
+            }}
+            type={capeVisibility ? 'default' : 'primary'}
+          >
+            {capeVisibility ? 'Скрыть' : 'Показать'}
+          </Button>
+        </Panel>
+      </Collapse>
+      <Button
+        onClick={() => setAutoRotationEnabled(autoRotationEnabled ? false : true)}
+        type={autoRotationEnabled ? 'default' : 'primary'}
+        style={{
+          position: 'fixed',
+          bottom: '20px',
+          left: '50%',
+          transform: 'translateX(-50%)'
+        }}
+      >
+        {autoRotationEnabled ? 'Остановить' : 'Вращать'}
+      </Button>
     </>
   );
 }
