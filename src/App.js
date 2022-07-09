ddd6007d300ceec92e617deafb828634b0bdace8
Arseny Kotkov <arsenykotkov@gmail.com>
2022-07-09 15:32:46 +1000

    Add canvas and mesh.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 
-function App() {
+export default function App() {
+  const ref = useRef();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <mesh ref={ref}>
+        <boxGeometry attach={'geometry'} args={[2, 2, 2]} />
+      </mesh>
+    </Canvas>
   );
 }
-
-export default App;
